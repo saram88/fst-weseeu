c9c9ec1d933b3df0f440d1aba2e41c2a2a96dd3b
Sara Mentzer <sara_mentzer@hotmail.com>
2023-07-29 12:09:10 +0000

    Update setting

--- django_weseeu/settings.py
+++ django_weseeu/settings.py
@@ -149,7 +149,7 @@ DATETIME_INPUT_FORMATS = [
 MEDIA_URL = '/media/'
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'weseeu/static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
---
+++ weseeu/static/JS/sendEmail.js
@@ -0,0 +1,14 @@
+function sendMail(contactForm) {
+    emailjs.send('service_vg0sszf', 'template_4p3bsdl', 
+    {
+      'from_name': contactForm.contact_name.value,
+      'from_email': contactForm.contact_epost.value,
+      'message': contactForm.contact_message.value
+    })
+    .then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+     }, function(error) {
+        console.log('FAILED...', error);
+     });
+     return false;
+}
